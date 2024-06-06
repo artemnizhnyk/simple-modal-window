1a348e7380d94d0a2d3faf43d4ec1bde60c79493
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-06 17:00:42 +0200

    open and close functionalities were added

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+"use strict";
+
+const modal = document.querySelector('.modal'),
+    overlay = document.querySelector('.overlay'),
+    btnCloseModal = document.querySelector('.close-modal'),
+    btnsShowModal = document.querySelectorAll('.show-modal');
+
+function openModal() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnsShowModal.forEach(e => e.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+
